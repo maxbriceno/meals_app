f88f8714783960549c8a9c2e890ca42e522f328a
maxbrice <briceno.massimo@emotion-team.com>
2024-10-10 19:28:37 +0200

    added meal model

---
+++ lib/data/meal.dart
@@ -0,0 +1,35 @@
+enum Complexity {
+  simple,
+  challenging,
+  hard,
+}
+
+enum Affordability {
+  affordable,
+  pricey,
+  luxurious,
+}
+
+class Meal {
+  const Meal({
+    required this.id,
+    required this.categories,
+    required this.title,
+    required this.imageUrl,
+    required this.ingredients,
+    required this.steps,
+    required this.duration,
+    required this.complexity,
+    required this.affordability,
+    required this.isGlutenFree,
+    required this.isLactoseFree,
+    required this.isVegan,
+    required this.isVegetarian,
+  });
+  final String id, title, imageUrl;
+  final List<String> categories, ingredients, steps;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree, isLactoseFree, isVegan, isVegetarian;
+}
